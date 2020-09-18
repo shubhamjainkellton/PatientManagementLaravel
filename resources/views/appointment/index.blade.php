094bd36540456459c510daff60ff29aeef59306a
Ayush <ayush.choudhary@kelltontech.com>
2020-09-18 15:47:48 +0530

    Upadtion in view appointment index page

--- resources/views/appointment/index.blade.php
+++ resources/views/appointment/index.blade.php
@@ -7,14 +7,16 @@
           <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
         </li>
         <li class="nav-item d-none d-sm-inline-block">
-          <a href="/admit_ward" class="nav-link">View Admitted Patient List</a>
+          <a href="/appointment" class="nav-link">View Appointment List</a>
         </li>
+      @if(session('role_id')==1 || session('role_id')==3)
         <li class="nav-item d-none d-sm-inline-block">
-          <a href="/admit_ward/create" class="nav-link">Admit Patient</a>
+          <a href="/appointment/create" class="nav-link">Add Appointment</a>
         </li>
+      @endif  
     </ul>
     <div class="col-md-4">
-    <form action="/admit_ward/search" method="get">
+    <form action="/appointment/search" method="get">
       <div class="input-group">
         <input type="search" name="search" class="form-control">
         <span class="input-group-btn">
@@ -40,7 +42,7 @@
           <div class="col-auto">
             <div class="card card-primary">
               <div class="card-header">
-                <h3 class="card-title">Admitted Patient List</h3>
+                <h3 class="card-title">Appointment List</h3>
               </div>
               <!-- /.card-header -->
               <div class="card-body table-responsive p-0">
@@ -49,32 +51,39 @@
                   <tr>
                     <th scope="col">S. No.</th>
                     <th scope="col">Patient Name</th>
-                    <th scope="col">Ward Type</th>
                     <th scope="col">Doctor Name</th>
-                    <th scope="col">Discharge Patient</th>
+                    <th scope="col">Date</th>
+                    <th scope="col">Time</th>
+                    
                     @if(session('role_id')===1) <th scope="colspan = 2">Actions</th> @endif
                   </tr>
                 </thead>
                 <tbody>
                   
-                  @foreach($wardadmits ?? '' as $wardadmit)
-                  @if($wardadmit->status === 0)
+                  @foreach($appointments as $appointment)
+                  
                   <tr>
-                    
+                   @if(session('department') == $appointment->department) 
                     <th scope="row">{{++$i}}</th>
-                    <td>{{$wardadmit->patient_name}}</td>
-                    <td>{{$wardadmit->ward_type}}</td>
-                    <td>{{$wardadmit->doc_name}}</td> 
-                    <td>
-                      <a href="depart_ward/{{$wardadmit->id}}/create" class='update btn btn-warning btn-sm'><span class="glyphicon glyphicon-minus-sign"></span></a>
-                    </td>
+                    <td>{{$appointment->patient_name}}</td>
+                    <td>{{$appointment->doc_name}}</td>
+                    <td>{{$appointment->date}}</td>
+                    <td>{{$appointment->time}}</td>
+                    @endif
+                    @if(session('role_id')==1 || session('role_id')==3)
+                    <th scope="row">{{++$i}}</th>
+                    <td>{{$appointment->patient_name}}</td>
+                    <td>{{$appointment->doc_name}}</td>
+                    <td>{{$appointment->date}}</td>
+                    <td>{{$appointment->time}}</td>
+                    @endif
                     @if(session('role_id') === 1)
                     <td>
-                      <a href="admit_ward/{{$wardadmit->id}}/edit" class='update btn btn-warning btn-sm'><span class="glyphicon glyphicon-pencil"></span></a>
+                      <a href="appointment/{{$appointment->id}}/edit" class='update btn btn-warning btn-sm'><span class="glyphicon glyphicon-pencil"></span></a>
                     </td>  
                     
                     <td>
-                    <form action="admit_ward/{{$wardadmit->id}}" method="POST">
+                    <form action="appointment/{{$appointment->id}}" method="POST">
                       @csrf
                       @method('DELETE')
                       <button class="delete btn btn-danger btn-sm" type="submit"><span class="glyphicon glyphicon-trash"></span></button> 
@@ -82,16 +91,17 @@
                     </td>
                     @endif
                   </tr>
-                  @endif
+                  
                   @endforeach
+                
                 </tbody>
               </table>
+              </div>
               <!-- /.card-body -->
-          </div>
+            </div>
             <!-- /.card -->
-        </div>
+          </div>
       </div>
   </div>
-</div>
 </section>
 @endsection
